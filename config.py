841d379dc35ddd45bd334dd52e4c3a21fd3dd894
Travis Miller <travisamm@gmail.com>
2022-06-28 19:01:38 -0700

    Added SECRET_KEY and SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -8,20 +8,20 @@ if os.path.exists(dotenv_path):
 
 
 class Config:
-    SECRET_KEY = #TODO - SETUP
+    SECRET_KEY = "b'\xdd\xa7\xdb\xb9/\x84Ev\x07\xb0f\t?XZvC\xa8\xa3I\x8bR\x86m6y\xff\xa4l\x056}\xb9\xe4\x13\x85\x93\xc7\xed\x95\x94\xddl\x0e[\x90\x9dU\x13\xd7\xed\xacX\x93tO\x15tB\x8b\x16\xe1Zg'"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
+# Production and dev configs use the same database URI
 class DevelopmentConfig(Config):
     ENV = "development"
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = #TODO - SETUP Database
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://foo:admin2020@localhost/db?host=localhost?port=3306'
     SQLALCHEMY_ECHO = True
 
-
+    
 class ProductionConfig(Config):
     ENV = "production"
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = #TODO - SETUP Database
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://foo:admin2020@localhost/db?host=localhost?port=3306'
     SQLALCHEMY_ECHO = False
 
